0acdf4f2c55af278ece49cd7d886f6da20a5cf09
raven4241 <106502737+raven4241@users.noreply.github.com>
2022-09-15 13:33:52 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,19 +1,44 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, request, session, redirect, url_for, render_template, g
 import os
-
+ 
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-  return render_template('login.html')
-  
-@app.route('/', methods=['POST', 'GET'])
-def login():
-  if request.method=='POST':
-    if request.form['username']!=admin and request.form['password']!=admin:
-      return render_template('login.html')
-    else:
-      return render_template('index.html')
+app.secret_key = os.urandom(24)
+ 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        session.pop('user', None)
+ 
+        if request.form['password'] == 'password':
+            session['user'] = request.form['username']
+            return redirect(url_for('protected'))
+ 
+    return render_template('index.html')
+ 
+@app.route('/protected')
+def protected():
+    if g.user:
+        return render_template('protected.html')
+ 
+    return redirect(url_for('index'))
+ 
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
+ 
+@app.route('/getsession')
+def getsession():
+    if 'user' in session:
+        return session['user']
+ 
+    return 'Not logged in!'
+ 
+@app.route('/dropsession')
+def dropsession():
+    session.pop('user', None)
+    return 'Dropped!'
 
 if __name__ == '__main__':
     app.run()
