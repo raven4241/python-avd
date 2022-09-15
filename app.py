18eabcac42c8810ab0966191685d9291e25a1a6f
raven4241 <106502737+raven4241@users.noreply.github.com>
2022-09-15 13:55:11 +0530

    update app.py

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ user = ["admin:password", "user:user1"]
 username = request.form['username']
 password = request.form['password']
 
-@app.route('/', methods==['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
-  return render_template('login.html', info = 'Please Login')
-  if username not in user and password not in user:
-    return redirect(url_for('/'))
-  else:
-    return render_template('index.html')
+  if request.method=='POST':
+    return render_template('login.html', info = 'Please Login')
+    if username not in user and password not in user:
+      return redirect(url_for('/'))
+    else:
+      return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
