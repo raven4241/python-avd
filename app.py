b3ec963f5dde006fe0e4dbbc23dfcfbd153d368b
raven4241 <106502737+raven4241@users.noreply.github.com>
2022-09-15 17:56:51 +0530

    update app.py

--- app.py
+++ app.py
@@ -7,19 +7,19 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("login.html")
-database={'nachi':'123','james':'aac','karthik':'asdsf'}
+database={'nachi':'123'}
 
-@app.route('/form_login',methods=['POST','GET'])
+@app.route('/login',methods=['POST','GET'])
 def login():
-    name1=request.form['username']
+    user=request.form['username']
     pwd=request.form['password']
-    if name1 not in database:
+    if user not in database:
 	    return render_template('login.html',info='Invalid User')
     else:
-        if database[name1]!=pwd:
+        if database[user]!=pwd:
             return render_template('login.html',info='Invalid Password')
         else:
-	         return render_template('index.html',name=name1)
+	         return render_template('index.html',name=user)
 
 if __name__ == '__main__':
     app.run()
