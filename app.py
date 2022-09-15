c606811b5917721479ddce9e732793abce3c88e6
raven4241 <106502737+raven4241@users.noreply.github.com>
2022-09-15 16:25:38 +0530

    update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import Flask, request, session, redirect, url_for, render_template
 app = Flask(__name__)
 
 user = ["admin:password", "user:user1"]
-username = request.form['username']
-password = request.form['password']
-
+  
 @app.route('/', methods=['GET', 'POST'])
 def login():
+  return render_template('login.html', info = 'Please Login')
   if request.method=='POST':
-    return render_template('login.html', info = 'Please Login')
-    if username not in user and password not in user:
+    usr = request.form["username"]
+    pwd = request.form["password"]
+    if usr not in user and pwd not in user:
       return redirect(url_for('/'))
     else:
       return render_template('index.html')
