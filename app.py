48c89924e18df4d06d615710d2a32c8c9e89c6d5
raven4241 <106502737+raven4241@users.noreply.github.com>
2022-09-15 13:46:42 +0530

    update app.py

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import Flask, request, session, redirect, url_for, render_template
 app = Flask(__name__)
 
 user = ["admin:password", "user:user1"]
+username = request.form['username']
+password = request.form['password']
 
 @app.route('/', methods==['GET', 'POST'])
 def login():
   return render_template('login.html', info = 'Please Login')
+  if 
 
 if __name__ == '__main__':
     app.run()
