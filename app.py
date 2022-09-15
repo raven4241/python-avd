b43bd531c92b96adcc8b5fa220424889aa4bec25
raven4241 <106502737+raven4241@users.noreply.github.com>
2022-09-15 13:41:53 +0530

    update app.py

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask, request, session, redirect, url_for, render_template
  
 app = Flask(__name__)
 
+user = ["admin:password", "user:user1"]
 
+@app.route('/')
+def login():
+  return 
 
 if __name__ == '__main__':
     app.run()
