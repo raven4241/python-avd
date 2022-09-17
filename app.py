7b9b194e5e30ab6104d11af82a6f3203fc2d6121
raven4241 <106502737+raven4241@users.noreply.github.com>
2022-09-17 17:44:43 +0530

    update app.py

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ def logout():
     return render_template('login.html', info = 'Logged Out')
 
 uploads_dir = os.path.join('uploads')
-os.makedirs(uploads_dir)
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
